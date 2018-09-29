47fca444fa9e5e696163193d5efe3f49644e471a
David Lobe <davidlobe@Davids-MacBook-Pro.local>
2018-09-29 05:38:43 -0400

    First commit input challenge

---
+++ input_challege.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 29 04:50:58 2018
+
+@author: davidlobe
+"""
+
+name = input("Enter your name: ")
+
+age = int(input("Enter your age: "))
+
+print("You will be 100yrs old in ", 100 - age, "years")
--- input_challenge.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 29 04:34:56 2018
-
-@author: davidlobe
-"""
-
