727f43c669b71bb8b7a2df952042464b23153581
davidlobe <davidlobe@Davids-MacBook-Pro.local>
2018-10-01 21:31:19 -0400

    commit python multi_parameters function demo

---
+++ multiple_parameter_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  1 20:28:40 2018
+
+@author: davidlobe
+"""
+
+def multi_parameters(a,b,c):
+    result = a - b*c
+    print("Parameter a equals", a)
+    print("Parameter b equals", b)
+    print("Parameter c equals", c)
+    return result
+    
